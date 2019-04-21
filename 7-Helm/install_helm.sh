b20a035ab2ac15680ec5799360b9bb32cbb71e6f
Stephen Ball <stephen@rebelinblue.com>
2019-04-21 09:20:43 +0100

    Add helm

---
+++ 7-Helm/install_helm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export HELM_VERSION=v2.13.1
+export HELM_INSTALL_DIR=/usr/local/bin
+wget https://kubernetes-helm.storage.googleapis.com/helm-$HELM_VERSION-linux-arm.tar.gz
+tar xvzf helm-$HELM_VERSION-linux-arm.tar.gz
+sudo mv linux-arm/helm $HELM_INSTALL_DIR/helm
+rm -rf linux-arm helm-$HELM_VERSION-linux-arm.tar.gz
+helm list
+kubectl create -f rbac-config.yaml
+helm init --service-account tiller --tiller-image jessestuart/tiller
+kubectl patch deployment tiller-deploy -n kube-system --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm"}}}}}'
