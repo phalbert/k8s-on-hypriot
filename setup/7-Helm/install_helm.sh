2cf65e2e86a3e49eb24c07fca1d7cd318c0e451d
Stephen Ball <stephen@rebelinblue.com>
2019-05-03 21:15:51 +0100

    Cleanup

--- setup/7-Helm/install_helm.sh
+++ setup/7-Helm/install_helm.sh
@@ -8,7 +8,7 @@ sudo mv linux-arm/helm $HELM_INSTALL_DIR/helm
 rm -rf linux-arm helm-$HELM_VERSION-linux-arm.tar.gz
 
 # curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
-helm list
 kubectl apply -f rbac-config.yaml
 helm init --service-account tiller --tiller-image jessestuart/tiller
 kubectl patch deployment tiller-deploy -n kube-system --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm"}}}}}'
+helm list
