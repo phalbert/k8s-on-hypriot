901db334310daa2e46b87890ae78c39bbbe4c26f
Stephen Ball <stephen@rebelinblue.com>
2019-05-19 22:02:27 +0100

    Add additional prometheus jobs

--- setup/setup.sh
+++ setup/setup.sh
@@ -54,6 +54,7 @@ e_header "Installing Blackbox Exporter"
 kubectl -n infra apply -f 9-Monitoring/blackbox-exporter.yaml
 
 e_header "Installing Prometheus & Alert Manager"
+kubectl -n kube-system apply -f 9-Monitoring/services.yaml
 kubectl -n infra apply -f 9-Monitoring/prometheus.yaml
 kubectl -n infra apply -f 9-Monitoring/alertmanager.yaml
 
