720023b33c18fb64874bbc1a960b0c7df0398606
Stephen Ball <stephen.ball@lendinvest.com>
2019-05-20 13:45:26 +0100

    Changing alert routing

--- setup/9-Monitoring/prometheus-apply.sh
+++ setup/9-Monitoring/prometheus-apply.sh
@@ -3,3 +3,4 @@
 kubectl -n infra delete configmap prometheus-config
 kubectl -n infra create configmap prometheus-config --from-file=prometheus/
 kubectl -n infra apply -f prometheus.yaml
+kubectl -n infra delete pod -lapp=prometheus-server
