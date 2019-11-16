20b73eee30b398fbc582d4a58ed9e97ebeea5ef3
Stephen Ball <stephen@rebelinblue.com>
2019-11-16 22:45:10 +0000

    Remove helm

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,15 +12,15 @@ e_header "Creating namespaces"
 kubectl apply -f namespaces.yaml
 
 # Linode token
-#kubectl -n infra create secret generic linode-dynamic-dns --from-literal="token=FOO"
+#kubectl -n infra create secret generic linode-dynamic-dns --from-literal="token=$token"
 #kubectl -n infra delete secret linode-dynamic-dns
 
-# htpasswd -nb stephen password
-#kubectl -n infra create secret generic dashboards-auth --from-literal="auth=FOO"
-#kubectl -n logging create secret generic dashboards-auth --from-literal="auth=FOO"
-#kubectl -n monitoring create secret generic dashboards-auth --from-literal="auth=FOO"
-#kubectl -n apps create secret generic dashboards-auth --from-literal="auth=FOO"
-#kubectl -n vault create secret generic dashboards-auth --from-literal="auth=stephen:\$apr1\$KFEfOc1u\$zLttioP2mi3h7DnwA70tE."
+# account=$(htpasswd -nb stephen password)
+#kubectl -n infra create secret generic dashboards-auth --from-literal="auth=$account"
+#kubectl -n logging create secret generic dashboards-auth --from-literal="auth=$account"
+#kubectl -n monitoring create secret generic dashboards-auth --from-literal="auth=$account"
+#kubectl -n apps create secret generic dashboards-auth --from-literal="auth=$account"
+#kubectl -n vault create secret generic dashboards-auth --from-literal="auth=$account"
 #kubectl -n apps delete secret dashboards-auth
 #kubectl -n infra delete secret dashboards-auth
 #kubectl -n logging delete secret dashboards-auth
@@ -35,7 +35,6 @@ kubectl -n infra patch deployment nfs-client-provisioner -n infra --patch '{"spe
 e_header "Installing ExternalDNS"
 kubectl -n infra apply -f 2-ExternalDNS/
 
-# Traefik is included with k3s but it is version 1.7
 e_header "Installing Traefik"
 kubectl -n infra apply -f 3-Traefik/
 
@@ -48,42 +47,38 @@ kubectl -n kube-system apply -f 5-Metrics-server/
 e_header "Installing Cert Manager"
 kubectl apply -f 6-CertManager/
 
-e_header "Installing Helm"
-kubectl -n kube-system apply -f 7-Helm/
-kubectl -n kube-system patch deployment tiller-deploy --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm"}}}}}'
-
 e_header "Installing Loki & Promtail"
-kubectl -n logging create configmap promtail --from-file=8-Logging/promtail/
-kubectl -n logging apply -f 8-Logging/
+kubectl -n logging create configmap promtail --from-file=7-Logging/promtail/
+kubectl -n logging apply -f 7-Logging/
 
 e_header "Installing Node Exporter"
-kubectl -n kube-system apply -f 9-Monitoring/node-exporter.yaml
+kubectl -n kube-system apply -f 8-Monitoring/node-exporter.yaml
 
 e_header "Installing Kube State Metrics"
-kubectl -n kube-system apply -f 9-Monitoring/kube-state-metrics.yaml
+kubectl -n kube-system apply -f 8-Monitoring/kube-state-metrics.yaml
 
 e_header "Installing Blackbox Exporter"
-kubectl -n monitoring apply -f 9-Monitoring/blackbox-exporter.yaml
+kubectl -n monitoring apply -f 8-Monitoring/blackbox-exporter.yaml
 
 e_header "Installing Prometheus & Alert Manager"
-kubectl -n monitoring create configmap prometheus-config --from-file=9-Monitoring/prometheus/
-kubectl -n monitoring apply -f 9-Monitoring/alertmanager.yaml
-kubectl -n monitoring apply -f 9-Monitoring/prometheus.yaml
+kubectl -n monitoring create configmap prometheus-config --from-file=8-Monitoring/prometheus/
+kubectl -n monitoring apply -f 8-Monitoring/alertmanager.yaml
+kubectl -n monitoring apply -f 8-Monitoring/prometheus.yaml
 
 e_header "Installing Grafana"
-kubectl -n monitoring apply -f 9-Monitoring/grafana.yaml
+kubectl -n monitoring apply -f 8-Monitoring/grafana.yaml
 
 e_header "Installing Consul"
-kubectl -n vault apply -f 10-Secrets/consul.yaml
+kubectl -n vault apply -f 9-Secrets/consul.yaml
 
 e_header "Installing Forecastle"
-kubectl -n apps apply -f 11-Others/forecastle.yaml
+kubectl -n apps apply -f 10-Others/forecastle.yaml
 
 e_header "Installing Kubeview"
-kubectl -n apps apply -f 11-Others/kubeview.yaml
+kubectl -n apps apply -f 10-Others/kubeview.yaml
 
 e_header "Installing Linode Dynamic DNS update"
-kubectl -n infra apply -f 11-Others/dyndns.yaml
+kubectl -n infra apply -f 10-Others/dyndns.yaml
 
 e_header "Getting Dashboard Token"
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
