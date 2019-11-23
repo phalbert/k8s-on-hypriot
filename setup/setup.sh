8b38cab5d5a6c1379ac64f9ca427f87d8d6bbd69
Stephen Ball <stephen@rebelinblue.com>
2019-11-23 22:01:15 +0000

    Cleanup

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,16 +11,18 @@ e_header() {
 e_header "Creating namespaces"
 kubectl apply -f namespaces.yaml
 
+kubectl taint nodes master node-role.kubernetes.io/master="":NoSchedule
+
 # Linode token
-#kubectl -n infra create secret generic linode-dynamic-dns --from-literal="token=$token"
+#kubectl -n infra create secret generic linode-dynamic-dns --from-literal='token=token'
 #kubectl -n infra delete secret linode-dynamic-dns
 
-# account=$(htpasswd -nb stephen password)
-#kubectl -n infra create secret generic dashboards-auth --from-literal="auth=$account"
-#kubectl -n logging create secret generic dashboards-auth --from-literal="auth=$account"
-#kubectl -n monitoring create secret generic dashboards-auth --from-literal="auth=$account"
-#kubectl -n apps create secret generic dashboards-auth --from-literal="auth=$account"
-#kubectl -n vault create secret generic dashboards-auth --from-literal="auth=$account"
+#htpasswd -nb stephen password
+#kubectl -n infra create secret generic dashboards-auth --from-literal='auth=authstring'
+#kubectl get secret dashboards-auth --namespace=infra --export -o yaml | kubectl apply --namespace=logging -f -
+#kubectl get secret dashboards-auth --namespace=infra --export -o yaml | kubectl apply --namespace=monitoring -f -
+#kubectl get secret dashboards-auth --namespace=infra --export -o yaml | kubectl apply --namespace=apps -f -
+#kubectl get secret dashboards-auth --namespace=infra --export -o yaml | kubectl apply --namespace=vault -f -
 #kubectl -n apps delete secret dashboards-auth
 #kubectl -n infra delete secret dashboards-auth
 #kubectl -n logging delete secret dashboards-auth
@@ -32,8 +34,8 @@ kubectl -n infra apply -f 1-NFS_Storage/
 kubectl -n infra patch storageclass nfs-hdd -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl -n infra patch deployment nfs-client-provisioner -n infra --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm"}}}}}'
 
-e_header "Installing ExternalDNS"
-kubectl -n infra apply -f 2-ExternalDNS/
+#e_header "Installing ExternalDNS"
+#kubectl -n infra apply -f 2-ExternalDNS/
 
 e_header "Installing Traefik"
 kubectl -n infra apply -f 3-Traefik/
@@ -72,8 +74,16 @@ kubectl -n monitoring apply -f 8-Monitoring/prometheus.yaml
 e_header "Installing Grafana"
 kubectl -n monitoring apply -f 8-Monitoring/grafana.yaml
 
-e_header "Installing Consul"
-kubectl -n vault apply -f 9-Secrets/consul.yaml
+#e_header "Installing Consul"
+#kubectl -n vault apply -f 9-Secrets/consul.yaml
+#
+#e_header "Installing Vault"
+#kubectl -n vault create secret generic vault-unseal-keys --from-literal="VAULT_UNSEAL_KEY_1=?" \
+#                                                         --from-literal="VAULT_UNSEAL_KEY_2=?" \
+#                                                         --from-literal="VAULT_UNSEAL_KEY_3=?" \
+#                                                         --from-literal="VAULT_UNSEAL_KEY_4=?" \
+#                                                         --from-literal="VAULT_UNSEAL_KEY_5=?"
+#kubectl -n vault apply -f 9-Secrets/vault.yaml
 
 e_header "Installing Forecastle"
 kubectl -n apps apply -f 10-Others/forecastle.yaml
@@ -84,14 +94,6 @@ kubectl -n apps apply -f 10-Others/kubeview.yaml
 
 e_header "Installing Linode Dynamic DNS update"
 kubectl -n infra apply -f 10-Others/dyndns.yaml
-
-e_header "Installing Vault"
-kubectl -n vault create secret generic vault-unseal-keys --from-literal="VAULT_UNSEAL_KEY_1=?" \
-                                                         --from-literal="VAULT_UNSEAL_KEY_2=?" \
-                                                         --from-literal="VAULT_UNSEAL_KEY_3=?" \
-                                                         --from-literal="VAULT_UNSEAL_KEY_4=?" \
-                                                         --from-literal="VAULT_UNSEAL_KEY_5=?"
-kubectl -n vault apply -f 9-Secrets/vault.yaml
 
 e_header "Getting Dashboard Token"
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
