c570bb00dc0e18b76a9ba66ea7ff8eb891b933ac
Stephen Ball <stephen@rebelinblue.com>
2019-11-28 14:27:17 +0000

    Add flux

---
+++ setup/10-Others/flux.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+kubectl create namespace flux
+helm repo add fluxcd https://charts.fluxcd.io
+helm upgrade --install flux --values ./flux-values.yaml --namespace flux fluxcd/flux
+
+#kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
