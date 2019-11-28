05e6cdbb07902984f4e75eb40c257f3e25592372
Stephen Ball <stephen@rebelinblue.com>
2019-11-28 17:41:48 +0000

    Add prometheus configmap

--- setup/configmaps/prometheus-apply.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kubectl -n monitoring delete configmap prometheus-config
-kubectl -n monitoring create configmap prometheus-config --from-file=prometheus/
-kubectl -n monitoring delete pod -lapp=prometheus-server
