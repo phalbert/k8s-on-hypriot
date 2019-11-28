f08f290aa73874b8a6b3ea68b7f876c878e3a8a2
Stephen Ball <stephen@rebelinblue.com>
2019-11-28 16:41:55 +0000

    Add logging namespace

--- old/setup/7-Logging/promtail-apply.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kubectl -n logging delete configmap promtail
-kubectl -n logging create configmap promtail --from-file=promtail/
-kubectl -n logging apply -f promtail.yaml
-kubectl -n logging delete pod -lk8s-app=promtail
