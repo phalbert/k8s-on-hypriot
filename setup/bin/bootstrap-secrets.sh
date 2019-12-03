5ee6143ce6350730e632763a42a64c66e83a3328
Stephen Ball <stephen@rebelinblue.com>
2019-12-03 00:02:07 +0000

    Use forward auth

--- setup/bin/bootstrap-secrets.sh
+++ setup/bin/bootstrap-secrets.sh
@@ -6,7 +6,6 @@ source "$REPO_ROOT/setup/.env"
 kubectl -n infra create secret generic linode-dynamic-dns --from-literal="token=$LINODE_TOKEN"
 
 AUTHSTRING=$(htpasswd -nb $AUTH_USERNAME $AUTH_PASSWORD)
-kubectl -n infra create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
 kubectl -n logging create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
 kubectl -n monitoring create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
 kubectl -n apps create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
