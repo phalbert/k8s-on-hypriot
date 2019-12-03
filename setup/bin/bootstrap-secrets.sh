76e0b34f934be81c991b59cb1d11e814d7d6d056
Stephen Ball <stephen.ball@lendinvest.com>
2019-12-03 10:32:10 +0000

    Switch to OpenID auth

--- setup/bin/bootstrap-secrets.sh
+++ setup/bin/bootstrap-secrets.sh
@@ -5,12 +5,6 @@ source "$REPO_ROOT/setup/.env"
 
 kubectl -n infra create secret generic linode-dynamic-dns --from-literal="token=$LINODE_TOKEN"
 
-AUTHSTRING=$(htpasswd -nb $AUTH_USERNAME $AUTH_PASSWORD)
-kubectl -n logging create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
-kubectl -n monitoring create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
-kubectl -n apps create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
-kubectl -n vault create secret generic dashboards-auth --from-literal="auth=$AUTHSTRING"
-
 kubectl -n vault delete secret vault-unseal-keys || true
 kubectl -n vault create secret generic vault-unseal-keys --from-literal="VAULT_UNSEAL_KEY_1=$VAULT_UNSEAL_KEY_1" \
                                                          --from-literal="VAULT_UNSEAL_KEY_2=$VAULT_UNSEAL_KEY_2" \
